f93668048b13e0c4c2b2fb819bb10e39697acaef
Murdock135 <particlegriffin@gmail.com>
2021-02-25 12:01:23 +0600

    Initial commit

---
+++ first.py
@@ -0,0 +1,69 @@
+#variables
+# print(3)
+# print(35) #int literal
+
+# print(35.59) #float literal
+
+# int1 = 3
+# int2 = 35
+# float1 = 35.59
+
+# sum = int1*int2
+# print(sum, end = '') #escape sequence
+
+# exponent1 = int1**int2
+# print(exponent1)
+
+# int1, int2 = 3,35
+# print(int1, int2)
+
+#ints, floats, print, operators, escape sequence
+
+
+# zayan = False #bools
+# ahnaf = True #boolean data type
+
+# comp = 3+2j #complex number made with literals
+# comp2 = int1 + int2*j #complex number NOT made with variables
+# print(comp, comp2)
+a = 'hello'
+b = 'world'
+c = "I am junayed"
+# d = "bye"
+# s1 = "zayan"
+# s2 = [a, b, c, 1, 2]
+# s3 = {a, d, 34, 76}
+#iterables
+'''iterables are datatypes that can be iterated over'''
+# print(s2[0])
+# print(s2[-1])
+# print(s2[3])
+
+# print(s2[0])
+
+'''summary
+1. python reads code line by line
+2. string
+3. list
+4. tuple'''
+
+# dict1 = { 'car':'BMW', 'number':127}
+
+# dict1.update({'wheels': 4}) 
+
+# print(dict1)
+
+# string1 = 'My name is\v Zayan'
+# string2 = 'I am Maz\'s son'
+# print(string1)
+# print(string2)
+
+# print('my name is Zayan' + 'I am 21')
+# print(a+b+c)
+
+
+abc = a+b+c
+print(abc)
+
+# abc = a*b
+# print(abc)
---
+++ strformatting.py
@@ -0,0 +1,12 @@
+age1 = input('What is your age')
+age2 = int(input('What is your age'))
+
+print("Your age is", age1)
+print("Your age is ", age2)#method1
+
+print(f"your age is {age1}")#method2
+print('your age is {}'.format(age1))#method3
+
+
+
+
