9f3f2c9d82ea2d1dd2965f64c6ed39a7799c9057
Murdock135 <particlegriffin@gmail.com>
2021-03-03 21:17:16 +0600

    removed code to get input

--- class4.py
+++ class4.py
@@ -3,18 +3,21 @@ def two_sum(NumList):
     listOfNums = NumList
     count = 0
 
-    while count<5:
-        string_nums = input('Enter a digit: ')
-        nums = int(string_nums) #convert to int
+    # while count<5:
+    #     string_nums = input('Enter a digit: ')
+    #     nums = int(string_nums) #convert to int
 
 
     target = int(input('What\'s the target'))
 
     listLength = len(listOfNums)
 
-    for m in range(0, listLength-1):
-        for n in range(m+1, listLength-1):
+    for m in range(0, listLength):
+        for n in range(m+1, listLength):
             if listOfNums[m]==listOfNums[n]:
                 continue
             elif listOfNums[m]+listOfNums[n]==target:
                 print(m,n)
+
+answer = two_sum([3,2,4,5,11])
+print(answer)
