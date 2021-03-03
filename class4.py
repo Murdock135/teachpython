514d867663c7cf997a666411ecd1a73de40efcb7
Murdock135 <particlegriffin@gmail.com>
2021-03-03 16:21:30 +0600

    sample code for class4

---
+++ class4.py
@@ -0,0 +1,20 @@
+def two_sum(NumList):
+    # listOfNums = []
+    listOfNums = NumList
+    count = 0
+
+    while count<5:
+        string_nums = input('Enter a digit: ')
+        nums = int(string_nums) #convert to int
+
+
+    target = int(input('What\'s the target'))
+
+    listLength = len(listOfNums)
+
+    for m in range(0, listLength-1):
+        for n in range(m+1, listLength-1):
+            if listOfNums[m]==listOfNums[n]:
+                continue
+            elif listOfNums[m]+listOfNums[n]==target:
+                print(m,n)
